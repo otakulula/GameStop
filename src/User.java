7df853efc1a1828c6916146ebf02953b04e8a099
Joshua Darlucio <joshuad26@icloud.com>
2025-03-16 11:58:41 -0700

    Created User class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,58 @@
-public class User {
-  
+/**
+ * User.java
+ * @author Joshua Darlucio
+ * CIS 22C, Final Project
+ */
+
+ public class User {
+    protected String firstName;
+    protected String lastName;
+    protected String email;  
+    protected String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (" + email + ")";
+    }
 }
