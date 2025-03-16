68887942f13f5926f80ad305d7518b62bb97ed59
Joshua Darlucio <joshuad26@icloud.com>
2025-03-16 13:05:46 -0700

    Added Javadocs to User class

--- src/User.java
+++ src/User.java
@@ -1,15 +1,17 @@
-/**
- * User.java
- * @author Joshua Darlucio
- * CIS 22C, Final Project
- */
-
 public class User {
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    /**
+     * Constructs a new User object with the given details.
+     * 
+     * @param firstName The first name of the user.
+     * @param lastName The last name of the user.
+     * @param email The email address of the user.
+     * @param password The password for the user.
+     */
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -17,40 +19,83 @@ public class User {
         this.password = password;
     }
 
-    // Getters
+    /**
+     * Retrieves the first name of the user.
+     * 
+     * @return The first name of the user.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Retrieves the last name of the user.
+     * 
+     * @return The last name of the user.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Retrieves the email address of the user.
+     * 
+     * @return The email address of the user.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Retrieves the password for the user.
+     * 
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
 
-    // Setters
+    /**
+     * Sets the first name of the user.
+     * 
+     * @param firstName The first name to set.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Sets the last name of the user.
+     * 
+     * @param lastName The last name to set.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Sets the email address for the user.
+     * 
+     * @param email The email to set.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Sets the password for the user.
+     * 
+     * @param password The password to set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Returns a string representation of the user, including the first name, last name, and email.
+     * 
+     * @return A string containing the user's first name, last name, and email.
+     */
     @Override
     public String toString() {
         return firstName + " " + lastName + " (" + email + ")";
