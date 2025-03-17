4365c28ca37c721403809a06fcf48bba224f88a4
HU3YN <117341624+HU3YN@users.noreply.github.com>
2025-03-16 22:43:21 -0700

    Made constructor, getter, and setters

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,39 @@
-public class Employee extends User{ 
+public class Employee extends User{
 
- //note user is an abstract class containing first name, last name, login, and password
+    private boolean isManager; 
 
-private boolean isManager; 
+    /**
+     * Constructs an Employee object with the given details.
+     * 
+     * @param firstName The first name of the employee.
+     * @param lastName The last name of the employee.
+     * @param email The email address of the employee.
+     * @param password The password for the employee.
+     * @param isManager Indicates if the employee is a manager.
+     */
+    public Employee(String firstName, String lastName, String username, String password, boolean isManager) {
+        super(firstName, lastName, username, password);
+        this.isManager = isManager;
+    }
 
-    //getters, setters, constructors go here
+    /**
+     * Retrieves whether the employee is a manager.
+     * 
+     * @return true if the employee is a manager, false otherwise.
+     */
+    public boolean getManager() {
+        return isManager;
+    }
+    
+    /**
+     * Sets whether the employee is a manager.
+     * 
+     * @param isManager true if the employee should be a manager, false otherwise.
+     */
+    public void setManager(boolean isManager) {
+        this.isManager = isManager;
+    }
+
 
  
 
