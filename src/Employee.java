672ea3e63547ba1477eafc1e2867bf1e17daf100
HU3YN <117341624+HU3YN@users.noreply.github.com>
2025-03-20 00:03:44 -0700

    Update search by ID and Name

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,8 @@
 public class Employee extends User{
 
     private boolean isManager; 
+    private Queue<Order> unshippedOrders;
+    private LinkedList<Order> shippedOrders;  
 
     /**
      * Constructs an Employee object with the given details.
@@ -14,6 +16,8 @@ public class Employee extends User{
     public Employee(String firstName, String lastName, String username, String password, boolean isManager) {
         super(firstName, lastName, username, password);
         this.isManager = isManager;
+        this.unshippedOrders = new Queue<>();
+        this.shippedOrders = new LinkedList<>();
     }
 
     /**
@@ -32,6 +36,61 @@ public class Employee extends User{
      */
     public void setManager(boolean isManager) {
         this.isManager = isManager;
+    }
+
+    /**
+     * Search for an order by ID.
+     * @param orderId is the ID to be searched
+     * @return order we are searching for
+     */
+    public Order searchOrderById(int orderID) {
+        Queue<Order> temp = new Queue<>(unshippedOrders);
+        while (!temp.isEmpty()) {
+            Order order = temp.getFront();
+            if (order.getID() == orderId) {
+                return order;
+            }
+            else{
+                temp.dequeue();
+            }
+        }
+        return null;
+    }
+    /**
+     * Search for an order by customer's first and last name.
+     * @param firstName is the first name of the customer
+     * @param lastName is the last name of the custormer
+     * @return order we are looking for
+     */
+
+    public Order searchOrderByCustomerName(String firstName, String lastName) {
+        Queue<Order> temp = new Queue<>(unshippedOrders);
+        while (temp.isEmpty()) {
+            Order order = temp.getFront();
+            if (order.getCustomer().getFirstName().equals(firstName) &&
+                order.getCustomer().getLastName().equals(lastName)) {
+                return order;
+            }
+            else{
+                temp.dequeue();
+            }
+        }
+        return null;
+    }
+    /**
+     * View the next order in the queue 
+     * @return the next order in the queue
+     */
+
+    public Order viewHighestPriorityOrder() {
+        return unshippedOrders.getFront();
+    }
+
+    /**
+     * View all orders (heap sort?)
+     */
+    public void viewAllOrders() {
+
     }
 
 
