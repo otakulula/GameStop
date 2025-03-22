88915a968972790f91889113e3a3afceb83ad20f
Daniella Rand <daniellarand28@gmail.com>
2025-03-22 01:49:58 -0700

    Updated test w/ basics

--- src/Order.java
+++ src/Order.java
@@ -3,10 +3,33 @@
   * @author Daniella Rand
   * CIS 22C, Final Project
   */
-public class Order {
 
-  public Order (){
-    System.out.println("test");
+import java.util.Comparator;
+import DataStructures.LinkedList;
+
+public class Order {
+	private int orderID;
+	private Customer customer;
+	private String date;
+	private LinkedList<VideoGame> orderContents;
+	private int shippingSpeed; // or use enums
+	private int priority;
+	
+	
+	/*
+	 * getters, setters, constructors go here
+	 * 
+	 * priority que needed
+	 * orders stored in order of priority in a priority queue that is based on a heap data 
+	 * structure
+	 * inserts, sorts, and removes according to an elements priority
+	 * 
+	 * priority is to be assigned according to the date and time of the order and the
+	 * shipping speed selected by the customer
+	 * */
+	
+   public Order (){
+	   System.out.println("test");
    }
 
   
